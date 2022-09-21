2142bc3ae029efd4e037430fec2d6af42cbc39dc
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2022-09-21 13:55:59 -0400

    Update bot.js

--- Bot.js
+++ Bot.js
@@ -1,1 +1,16 @@
+const { Client, Collection, GatewayIntentBits } = require(`discord.js`);
+const fs = require(`fs`);
+
+const {Token} = require("./config.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions
+  ]
+});
 

c5b0771f62d7150d3968593deabd7a6ac993c9a7
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2022-09-21 13:26:02 -0400

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,2 +1,4 @@
-const Token = "BOT-TOKEN-GOES-HERE"
+const Token = "BOT-TOKEN-GOES-HERE" //The bot's token.
 const CommandType = "Both" //Can be Slash, Prefix, or Both. If anything else is put in, Slash will automatically load.
+
+const Prefix = "!" //Prefix command prefix.

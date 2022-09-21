db67a9c8b008ca3bc5facecdfdf8633342ad7f64
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2022-09-21 15:26:49 -0400

    Create basic functionality i think

--- Bot.js
+++ Bot.js
@@ -14,3 +14,35 @@ const client = new Client({
   ]
 });
 
+client.commands = new Collection();
+client.commandArray = [];
+
+const cmdfiles = fs.readdirSync(`./Commands`).filter(file => file.endsWith(`.js`));
+for (const f of cmdfiles) {
+  const command = require(`./Commands/${f}`)
+  commands.set(command.data.name, command);
+  commandArray.push(command.data.toJSON())
+  console.log("Registering command " + command.data.name + ".")
+}
+const rest = new REST({ version: `9` }).setToken(process.env.token)
+try {
+  await rest.put( Routes.applicationCommands(clientId), {
+    body: commandArray,
+  })
+} catch (error) {
+  console.error(error)
+}
+
+const eventFolders = fs.readdirSync("./Events").filter((file) => file.endsWith(".js"));
+for(const file of eventFiles) {
+  const event = require(`./Events/${file}`)
+  if(event.once) client.once(event.name, (...args) => event.execute(...args, client));
+  else client.on(event.name, (...args) => event.execute(...args, client));
+}
+
+client.emojilib = {
+  fazcoin = "<:fazcoin:1015807386826584064>",
+  fredrick_fazbot = "<:fredrick_fazbot:1015801229160501308>"
+}
+
+client.login(token);

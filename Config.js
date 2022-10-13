2b7f1fcb5831b4a94fbf24ec2fe694eeb3f29044
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2022-10-12 20:09:31 -0400

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,4 +1,4 @@
-this.Token = process.env['token'] //The bot's token.
+this.Token = "BOT-TOKEN-HERE"  //The bot's token.
 this.ClientId = "1015641148066369538" // PUT THE BOT CLIENT/APPLICATION ID HERE OR IT WON'T WORK.
 
 this.CommandType = "Both" //Can be Slash, Prefix, or Both. If anything else is put in, Slash will automatically load.
